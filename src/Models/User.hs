3752c0951f9317327cbe798763a1e8ae9bfe607b
Zarak <zarak@hotmail.ca>
2020-06-18 21:33:02 +0500

    Add user model and queries

---
+++ src/Models/User.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Models.User where
+
+import qualified Opaleye as O
+import Control.Monad (mzero)
+import Data.Aeson
+import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Profunctor.Product.TH (makeAdaptorAndInstance)
+
+import App
+
+data User' email pwd = User
+    { userEmail :: email
+    , userPassword :: pwd
+    }
+
+-- Use polymorphic type User' on both Haskell User type and postgres-opaleye
+-- User type
+type User = User' Email ByteString
+type UserColumn = User' (O.Column O.PGText) (O.Column O.PGBytea)
+
+instance FromJSON User where
+    parseJSON (Object o) = User <$>
+        o .: "email" <*>
+            (BS.pack <$> o .: "password")
+    parseJSON _ = mzero
+
+-- This TH creates a product profunctor
+$(makeAdaptorAndInstance "pUser" ''User')
+
+-- Read and Write have separate types - but here they are the same, thus UserColumn is repeated
+userTable :: O.Table UserColumn UserColumn
+userTable = O.Table "users" (pUser User
+    { userEmail = O.required "email"
+    , userPassword = O.required "password"
+    })
+
+userToPG :: User -> UserColumn
+userToPG = pUser User
+    { userEmail = O.pgString
+    , userPassword = O.pgStrictByteString
+    }
---
+++ src/Queries/User.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE Arrows #-}
+
+module Queries.User where
+
+import qualified Opaleye as O
+import Opaleye ((.==))
+import Control.Arrow (returnA)
+
+import App
+import Models.User
+
+usersQuery :: O.Query UserColumn
+usersQuery = O.queryTable userTable
+
+userByEmailQuery :: Email -> O.Query UserColumn
+userByEmailQuery email = proc () -> do
+    user <- usersQuery -< ()
+    O.restrict -< userEmail user .== O.pgString email
+    returnA -< user
